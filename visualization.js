fa79e9de6f21157bcb1e80d1987a2b5af4256b61
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-07-03 09:57:39 +0200

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -1,36 +1,81 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+    d3.json("/countries.json").then(countries => {
+        d3.csv("/cities.csv").then(cities => {
+            // Geografische Formen sind in `countries.features`
 
-  d3.json("/countries.json")
-    .then(countries => {
-      d3.csv("/cities.csv")
-        .then(cities => {
+            // Städtedaten sind in `cities`
 
-          // Geografische Formen sind in `countries.features`
+            // ========================================
 
-          // Städtedaten sind in `cities`
-          
-          // ========================================
+            // Definiere die Breite (width) und Höhe (height) deiner Visualisierung `800x600`
 
-          // Definiere die Breite (width) und Höhe (height) deiner Visualisierung `800x600`
+            const width = 800;
+            const height = 400;
 
-          // Hole den #viz container aus dem DOM mit `d3.select()`
+            // Hole den #viz container aus dem DOM mit `d3.select()`
 
-          // Erstelle eine SVG node mit den Dimensionen `800x600`
-    
-          // Konfiguriere eine Kartenprojektion
-    
-          // Erstelle einene Pfadgenerator mit `d3.geoPath()`
+            const container = d3.select("#viz");
 
-          // Optional: Erstelle ein Gradnetz mit `d3.geoGraticule()()`
+            // Erstelle eine SVG node mit den Dimensionen `800x600`
 
-          // Binde `countries.features` an Pfade
-    
-          // Binde `cities` an Kreise
+            const svg = container
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .style("background-color", "black");
 
-          // ========================================
+            // Konfiguriere eine Kartenprojektion
 
-        })
-    })
+            const projection = d3
+                .geoAlbers()
+                .rotate([-20.0, 0.0])
+                .center([0.0, 52.0])
+                .translate([width / 2, height / 2])
+                .scale(700);
 
-})
+            // Erstelle einene Pfadgenerator mit `d3.geoPath()`
+
+            const path = d3.geoPath().projection(projection);
+
+            // Optional: Erstelle ein Gradnetz mit `d3.geoGraticule()()`
+
+            const graticule = d3.geoGraticule()();
+
+            const graticulePath = svg
+                .append("path")
+                .attr("d", path(graticule))
+                .attr("stroke", "#DDD")
+                .attr("stroke-width", 0.5)
+                .attr("fill", "transparent");
+
+            // Binde `countries.features` an Pfade
+
+            const mycountries = svg
+                .selectAll("path")
+                .data(countries.features)
+                .enter()
+                .append("path")
+                .attr("d", d => path(d))
+                .attr("fill", "grey")
+                .attr("stroke", "#DDD")
+                .attr("stroke-width", 0.5);
+
+            // Binde `cities` an Kreise
+
+            const mycities = svg
+                .selectAll("circle")
+                .data(cities)
+                .enter()
+                .append("circle")
+                .attr("cx", d => projection([d.lon, d.lat])[0])
+                .attr("cy", d => projection([d.lon, d.lat])[1])
+                .attr("r", d => d.size)
+                .attr("fill", "lightgrey")
+                .on("click", d => {
+                    console.log("Data: ", d);
+                });
+
+            // ========================================
+        });
+    });
+});
